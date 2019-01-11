9823f186575ae7e01e1f5c8675bdbe7e80adb4b3
vulvullatin <34389362+vulvullatin@users.noreply.github.com>
2019-01-11 15:29:27 -0500

    Update ListAlarmActivity.java
    Fixed putting and receiving from sharedpreferences. started alarm implementation. fixed recyclerview

--- AlarmGame/app/src/main/java/com/example/a2019soshay/alarmgame/ListAlarmActivity.java
+++ AlarmGame/app/src/main/java/com/example/a2019soshay/alarmgame/ListAlarmActivity.java
@@ -12,12 +12,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-public class ListAlarmActivity extends AppCompatActivity extends Broadcas {
+public class ListAlarmActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
     private SharedPreferences mPrefs;
     private SharedPreferences.Editor mEditor;
-    private ArrayList<String> mAlarms;
+    private ArrayList<String> mNames;
     private ArrayList<String> mLabels;
     private ArrayList<Boolean> mButtons;
     private MyRecyclerViewAdapter adapter;
@@ -25,35 +31,86 @@ public class ListAlarmActivity extends AppCompatActivity extends Broadcas {
     private int mPositionDelete;
     private String mLabel;
     private boolean mDelete;
+    private ArrayList<ArrayList<String>> mEverything;
     private boolean mCameFromAlarm;
     private boolean mAlarmChange;
     private int mPosition;
+    private ArrayList<Alarm> mAlarms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_alarm);
         mPrefs = getSharedPreferences("label", 0);
-        mAlarms = mPrefs.getAll().get("my_alarms");
-        mLabels = mPrefs.getAll().get("my_labels");
-        mButtons = mPrefs.getAll().get("my_buttons");
+        //mAlarms = mPrefs.getAll().get("my_alarms");
+        //mLabels = mPrefs.getAll().get("my_labels");
+        //mButtons = mPrefs.getAll().get("my_buttons");
+
         mEditor = mPrefs.edit();
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.workshopParticipants);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        try {
+            mNames = (ArrayList<String>) ObjectSerializer.deserialize(mPrefs.getString("name_list", ObjectSerializer.serialize(new ArrayList<String>())));
+            mLabels = (ArrayList<String>) ObjectSerializer.deserialize(mPrefs.getString("label_list", ObjectSerializer.serialize(new ArrayList<String>())));
+            mButtons = (ArrayList<Boolean>) ObjectSerializer.deserialize(mPrefs.getString("button_list", ObjectSerializer.serialize(new ArrayList<Boolean>())));
+            mAlarms = (ArrayList<Alarm>) ObjectSerializer.deserialize(mPrefs.getString("alarm_list", ObjectSerializer.serialize(new ArrayList<Alarm>())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //mAlarms.add("6:30");
+        try {
+            mEditor.putString("name_list", ObjectSerializer.serialize(mNames));
+            mEditor.putString("label_list", ObjectSerializer.serialize(mLabels));
+            mEditor.putString("alarm_list", ObjectSerializer.serialize(mAlarms));
+            mEditor.putString("button_list", ObjectSerializer.serialize(mButtons));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        //mEditor.commit();
         mNewAlarm = getIntent().getBooleanExtra("new_alarm",false);
         mCameFromAlarm= getIntent().getBooleanExtra("came_from_alarm",false);
         if (mCameFromAlarm){
             mAlarmChange = getIntent().getBooleanExtra("alarm_changed",false);
             mPosition = getIntent().getIntExtra("position",-1);
+            if(mNewAlarm){
+                Alarm newAlarm= new Alarm();
+                String newName = getIntent().getStringExtra("name");
+                String newLabel = getIntent().getStringExtra("label");
+                Boolean on = getIntent().getBooleanExtra("on",false);
+                if (on){
+                    // do stuff after this
+                }
+            }
             if (mAlarmChange){
                 if ((mButtons.get(mPosition))==false){
-
+                    mAlarms.get(mPosition).cancelAlarm(this);
+                    mAlarms.remove(mPosition);
+                    mLabels.remove(mPosition);
+                    mNames.remove(mPosition);
+                    mButtons.remove(mPosition);
+                    mEverything.remove(mPosition);
                 }
-                    //cancel alarm
-                //if alarm turned on '' '' '' '':
-                    //start alarm
+                else{
+                    Calendar cal = Calendar.getInstance();
+                    SimpleDateFormat df = new SimpleDateFormat("KK:mm aa", Locale.US);
+                    Date d1=cal.getTime();
+                    try{
+                        d1 = df.parse(mNames.get(mPosition));
+                    }
+                    catch (ParseException e){
+                        e.printStackTrace();
+                    }
+                    Calendar c1 = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                    c1.setTime(d1);
+                    mAlarms.get(mPosition).setAlarm(this, cal);
+                }
+
             }
         }
-        adapter = new MyRecyclerViewAdapter(this, myAlarm); //what list should i use
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.workshopParticipants);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(ListAlarmActivity.this, mEverything); //what list should i use
         adapter.setClickListener(this);//This has a solution i'll ask later
         recyclerView.setAdapter(adapter);
 
